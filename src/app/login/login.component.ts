91a610de2dfff922c56fb3324d918f69f7ca330a
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-07-28 16:11:14 +0200

    Added jsdoc for login.comp

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,9 +14,17 @@ export class LoginComponent implements OnInit {
   rememberMeControl = new FormControl(false);
 
 
+/**
+ * Constructor for LoginComponent
+ * @param {AuthService} authService - An instance of AuthService
+ */
   constructor(private authService: AuthService) { }
 
 
+/**
+ * Angular lifecycle hook that is called after data-bound properties of a directive are initialized.
+ * Initializes the login form and sets up a delay for stopping an animation.
+ */
   ngOnInit(): void {
     setTimeout(() => {
       this.stopAnimation();
@@ -24,39 +32,41 @@ export class LoginComponent implements OnInit {
 
     const rememberedEmail = localStorage.getItem('email');
     this.rememberMeControl.setValue(rememberedEmail !== null);
-  
+
     this.loginForm = new FormGroup({
-      email: new FormControl(rememberedEmail || '', Validators.required),
+      email: new FormControl(rememberedEmail || '', Validators.email),
       password: new FormControl('', Validators.required)
     });
   }
 
+
+/**
+ * Stops the loading animation by setting `animationStopped` property to true.
+ * @private
+ */
   private stopAnimation(): void {
     this.animationStopped = true;
   }
 
 
+/**
+ * Handles the submit action of the login form.
+ * If form is valid, calls AuthService's signIn method with the entered email and password.
+ * If "remember me" is checked, stores the email in localStorage.
+ */
   onSubmit() {
-    if (!this.loginForm.valid) {
-      return;
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.rememberMeControl.value ? localStorage.setItem('email', email) : localStorage.removeItem('email');
+      this.authService.signIn(email, password).catch(() => this.isEmailPasswordInvalid = true);
     }
-  
-    const email = this.loginForm.get('email')!.value;
-    const password = this.loginForm.get('password')!.value;
-  
-    if(this.rememberMeControl.value) {
-      localStorage.setItem('email', email);
-    } else {
-      localStorage.removeItem('email');
-    }
-  
-    this.authService.signIn(email, password)
-      .catch(() => {
-        this.isEmailPasswordInvalid = true; 
-      });
   }
 
 
+/**
+ * Handles the guest login action.
+ * Calls AuthService's signInAnonymously method.
+ */
   onGuestLogin() {
     this.authService.signInAnonymously();
   }
